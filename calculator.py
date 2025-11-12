46afc4b131d6c5e8add41838d9402b4c4af7f312
nataliaveltri <natyveltri@icloud.com>
2025-11-12 12:22:36 -0500

    added functions aa, sub, mul, div, log and exp

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -6,6 +7,24 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a + b
 
+def sub (a, b):
+    return a - b
 
+def mul (a, b):
+    return a*b
+
+def div (a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b/a
+
+def log(a,b):
+    if a<= 0 or b <=0 or b==1:
+        raise ValueError
+    return math.log(a,b)
+
+def exp(a,b):
+    return a**b
